2a13ba44c55cc9d377825d94b793be4a0193815d
Christian Steimel <me@C14L.com>
2015-11-17 12:57:28 +0100

    setup basic admin

--- dtr5app/admin.py
+++ dtr5app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Profile, Flag, Sr, Subscribed
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class SrAdmin(admin.ModelAdmin):
+    pass
+
+
+class SubscribedAdmin(admin.ModelAdmin):
+    pass
+
+
+class FlagAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Sr, SrAdmin)
+admin.site.register(Subscribed, SubscribedAdmin)
+admin.site.register(Flag, FlagAdmin)
