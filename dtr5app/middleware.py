3e5e62c853b01000d8d54fe8e66b388929de790a
Christian Steimel <me@C14L.com>
2015-12-01 21:55:42 +0100

    pep8 fix

--- dtr5app/middleware.py
+++ dtr5app/middleware.py
@@ -76,8 +76,8 @@ class UserSetDefaultLocalizationValues():
     def process_request(self, request):
         if request.user.is_authenticated():
             if not request.user.profile.pref_distance_unit:
-                # if no distance unit set by user, try to find one based on user's
-                # browser settings.
+                # if no distance unit set by user, try to find one based on
+                # user's browser settings.
                 lc = request.META['HTTP_ACCEPT_LANGUAGE']
                 lc = lc.split(';', 1)[0].split(',', 1)[0]
                 lc = re.sub(r'[^a-z]', '', lc.lower())
