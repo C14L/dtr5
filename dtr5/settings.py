6f93cdac09af2605b76c1c83877a3c309f559929
Christian Steimel <me@C14L.com>
2020-06-01 17:44:29 +0200

    settings: Fix static dir

--- dtr5/settings.py
+++ dtr5/settings.py
@@ -92,7 +92,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # =====================================
 # settings for "django-cors-headers"
