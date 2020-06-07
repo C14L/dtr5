b0d03d3aec2e3f0a0bf42eb5ca19152c5e13dcad
Christian Steimel <me@C14L.com>
2020-06-07 16:39:49 +0200

    upload.sh: Add nginx restart

--- .upload.sh
+++ .upload.sh
@@ -4,5 +4,8 @@
 
 SRC=$( cd "$( dirname "$0" )"; pwd )
 DST="${SERVER01}:/opt/"
+
 rsync -rtvP --delete --exclude=.git* ${SRC} ${DST}
+
+pass webdev/server01-chris | head -n1 | ssh -tt ${SERVER01} "sudo systemctl restart dtr5.service"
 
